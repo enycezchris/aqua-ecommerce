26a142b2ca3625e8e90048784a154ab06fbc1c99
enycezchris <enycezchris@gmail.com>
2023-05-03 17:24:49 -0400

    changed styles in carousel

--- src/components/utils/carouselData.js
+++ src/components/utils/carouselData.js
@@ -3,20 +3,28 @@ export const carouselData = [
     id: 1,
     category: "Freshwater Fish",
     img: "https://fishlab.com/wp-content/uploads/2022/09/24-Peaceful-Community-Fish-For-Your-Tank-With-Pics.jpg",
+    description: "Shop all freshwater aquarium fish",
+    bgColor: "#577d86",
   },
   {
     id: 2,
     category: "Saltwater Fish",
     img: "https://www.thesprucepets.com/thmb/5MzmhLkvnnY4LbV1iGlQdmtFm_Y=/4603x0/filters:no_upscale():strip_icc()/close-up-of-fish-swimming-in-aquarium-739292555-599373b29abed50010cb60f3.jpg",
+    description: "Shop all saltwater aquarium fish",
+    bgColor: "#577d86",
   },
   {
     id: 3,
     category: "Invertebrates",
     img: "https://entnemdept.ufl.edu/creatures/MISC/cherry_shrimp01.png",
+    description: "Shop all freshwater aquarium invertebrates",
+    bgColor: "#577d86",
   },
   {
     id: 4,
     category: "Aquatic plants",
     img: "https://www.treehugger.com/thmb/ADOGHXBKfRdie0uhahxQYW5xgJc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/landscape-scenery-in-the-fish-tank-1172944939-15e8aea0279044589fcc5f8154b44d4e.jpg",
+    description: "Shop all freshwater aquatic plants",
+    bgColor: "#577d86",
   },
 ];
